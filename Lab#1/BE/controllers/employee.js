b6eba27104bd32e67bad1a36f192bb724f320b58
YoussefDarwishh <youssef.darwish03@eng-st.cu.edu.eg>
2024-02-17 14:37:46 +0200

    Lab 1 Completed
    Lab 1 Completed

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,23 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  const id = req.params.id;
+  const index = employee.findIndex((emp) => emp.id === id);
+  if (index !== -1) {
+    employee.splice(index, 1);
+    res.status(200).json({ data: employee });
+  } else {
+    res.status(404).json({ message: 'Employee not found' });
+  }
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  const { id, name } = req.body;
+  const x = employee.findIndex((emp) => emp.id === id);
+  if (x !== -1) {
+    return res.status(400).json({ message: 'An employee with the given ID already exists.' });
+  }
+  employee.push({ id, name });
+  res.status(201).json({ data: employee });
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -27,26 +27,59 @@ function fetchEmployees() {
       })
     })
     .catch(error => console.error(error))
+
+  //add here event listener to delete button
+  document.getElementById('dataTable').addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON' && event.target.textContent === 'Delete') {
+      const id = event.target.closest('tr').children[0].textContent;
+      deleteEmployee(id);
+    }
+  });
 }
 
 // TODO
 // add event listener to submit button
-
+document.getElementById('employeeForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  createEmployee();
+});
 // TODO
 // add event listener to delete button
 
 // TODO
-function createEmployee (){
+function createEmployee() {
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  const name = document.getElementById('name').value;
+  const id = document.getElementById('id').value;
+  if (!name || !id) return alert('Please fill all fields');
+  fetch('http://localhost:3000/api/v1/employee', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, name }),
+  })
+    .then(response => response.json())
+    .then(() => {
+      fetchEmployees();
+      document.getElementById('name').value = '';
+      document.getElementById('id').value = '';
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee(id) {
   // get id
   // send id to BE
   // call fetchEmployees
+  fetch(`http://localhost:3000/api/v1/employee/${id}`, {
+    method: 'DELETE',
+  })
+    .then(response => response.json())
+    .then(() => fetchEmployees())
+    .catch(error => console.error('Error:', error));
 }
-
 fetchEmployees()
